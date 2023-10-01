d66835468fae937cf7e47cda64be9edda2fd9894
Ar-Akhil <ar.akhil1511@gmail.com>
2023-10-01 15:06:15 +0530

    added a new algo function

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function draw() {
     line(xArr[i], 490, xArr[i], yArr[i]);
   }
   bubbleSort();
-  //quickSort();
+  // quickSort();
 }
 
 function mousePressed() {
@@ -66,4 +66,45 @@ function quickSort() {
     "info"
   ).textContent = `Merge sort is the sorting technique that follows the 
   divide and conquer approach. Time complexity: O(n*logn)`;
+
+  function partition(arr, low, high) {
+    // Choosing the pivot
+    let pivot = arr[high];
+
+    // Index of smaller element and indicates the right position of pivot found so far
+    let i = low - 1;
+
+    for (let j = low; j <= high - 1; j++) {
+      // If current element is smaller than the pivot
+      if (arr[j] < pivot) {
+        // Increment index of smaller element
+        i++;
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+      }
+    }
+
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; // Swap pivot to its correct position
+    return i + 1; // Return the partition index
+  }
+
+  // The main function that implements QuickSort
+  function quickSort(arr, low, high) {
+    if (low < high) {
+      // pi is the partitioning index, arr[pi] is now at the right place
+      let pi = partition(arr, low, high);
+
+      // Separately sort elements before partition and after partition
+      quickSort(arr, low, pi - 1);
+      quickSort(arr, pi + 1, high);
+    }
+  }
+
+  // Driver code
+  let arr = [10, 7, 8, 9, 1, 5];
+  let N = arr.length;
+
+  // Function call
+  quickSort(arr, 0, N - 1);
+  console.log("Sorted array:");
+  console.log(arr.join(" "));
 }
